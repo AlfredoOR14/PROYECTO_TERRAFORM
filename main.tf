df839c5811f650d6dc337cb1e57b338098c8848d
AlfredoOR14 <alfredo051429@gmail.com>
2024-03-22 00:18:54 -0500

    ucreate

--- main.tf
+++ main.tf
@@ -1,8 +1,11 @@
 provider "aws" {
-  region = "us-west-2"
+  region = "us-east-1"  # Cambia la región según tus necesidades
 }
 
-resource "aws_instance" "ALFREDO" {
-  ami           = "ami-0c55b159cbfafe1f0"
-  instance_type = "t2.micro"
+resource "aws_ecr_repository" "example" {
+  name                 = "example-repo"  # Nombre del repositorio
+  image_tag_mutability = "MUTABLE"       # Etiquetas de imágenes mutables
+  image_scanning_configuration {
+    scan_on_push = true                   # Escaneo de imágenes al subirlas al repositorio
+  }
 }
